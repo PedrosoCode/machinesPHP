b6e79183aa343816186ff370ce2c71cd7a06025b
Gabriel <Gabrielpedrofc@gmail.com>
2024-04-20 16:06:59 -0300

    Ajuste conecta_banco

--- cadastrar.php
+++ cadastrar.php
@@ -1,30 +1,32 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "@Inspiron1";
-$dbname = "db_machines";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include('conecta_banco.php');
 
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
-
+// Primeiro, verificar se o e-mail já está registrado
+$sql = "SELECT id FROM usuarios WHERE email = ?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $nome, $email, $senha);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
 
-if ($stmt->execute()) {
-    session_start();
-    $_SESSION['nome'] = $nome;
-    header("Location: bem_vindo.php");
+if ($result->num_rows > 0) {
+    // E-mail já existe, retornar erro
+    echo "Erro: O e-mail fornecido já está em uso.";
 } else {
-    echo "Erro: " . $stmt->error;
+    // E-mail disponível, proceder com o cadastro
+    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $nome, $email, $senha);
+    if ($stmt->execute()) {
+        session_start();
+        $_SESSION['nome'] = $nome;
+        header("Location: bem_vindo.php");
+    } else {
+        echo "Erro ao cadastrar usuário: " . $stmt->error;
+    }
 }
 
 $stmt->close();
--- login.php
+++ login.php
@@ -1,14 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "@Inspiron1";
-$dbname = "db_machines";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include('conecta_banco.php');
 
 $email = $_POST['email'];
 $senha = $_POST['senha'];
